371230f459062a06ef22e50dd58b4808da11daa2
Jefferson Vinícius <vinicius.felipe1319@gmail.com>
2025-07-06 21:37:14 -0300

    Mecanica de adicionar itens ao carrinho atualizada!

--- scripts/addToCart.js
+++ scripts/addToCart.js
@@ -5,231 +5,139 @@ buttons.forEach((button) => {
 
 function addCart(event) {
     const buttonCurrent = event.currentTarget
-
-    if (buttonCurrent.id === "addwafle")
-    {
-        const selection = document.querySelector("div#selectionwafle")
+    const product = buttonCurrent.parentElement
+        
+        const empty = document.querySelectorAll(".viewempty")
+        empty.forEach((em) => {
+            em.style.display = "none"
+        })
+        const selection = product.querySelector("div.selection")
         selection.style.display = "flex"
+
+        const cart = document.querySelector("section#cart")
+        const title = cart.children[0]
+        const selectedProd = document.createElement("div")
+        const sectionDescri = document.createElement("section")
+        const descri = document.createElement("p")
+        const multi = document.createElement("p")
+        const priceCurrent = document.createElement("p")
+        const priceTotal = document.createElement("p")
+        const containerB = document.createElement("div")
+        const buttonRemove = document.createElement("button")
+        const imageButton = document.createElement("img")
+        const line = document.createElement("hr")
+
+        const quantityCartTotal = title.querySelector("p")
+        quantityCartTotal.textContent = Number(quantityCartTotal.textContent) + 1
+        
+        descri.textContent = product.querySelector("p.namecomplete").textContent
+        multi.textContent = "1x"
+        priceCurrent.textContent = "@" + product.querySelector("p.price").textContent
+        priceTotal.textContent = product.querySelector("p.price").textContent
+
+        selectedProd.id = "selected" + product.id
+        selectedProd.classList.add("selectedprod")
+        descri.classList.add("descri")
+        multi.classList.add("multi")
+        priceCurrent.classList.add("pricecurrent")
+        priceTotal.classList.add("pricetotal")
+        buttonRemove.classList.add("removeitem")
+        imageButton.setAttribute("src", "assets/images/icon-remove-item.svg")
+        imageButton.setAttribute("alt", "Remove Item")
+
+        sectionDescri.appendChild(descri)
+        sectionDescri.appendChild(multi)
+        sectionDescri.appendChild(priceCurrent)
+        sectionDescri.appendChild(priceTotal)
+        selectedProd.appendChild(sectionDescri)
+
+        buttonRemove.appendChild(imageButton)
+        containerB.appendChild(buttonRemove)
+        selectedProd.appendChild(containerB)
+
+        title.after(selectedProd)
+        selectedProd.after(line)
+
+        const buyMenuExists = cart.querySelector(".buymenu")
+
+        if (!buyMenuExists) {
+            const carbon = document.createElement("div")
+            const confirm = document.createElement("button")
+            const orderTotal = document.createElement("div")
+
+            orderTotal.classList.add("ordertotal", "buymenu")
+            carbon.classList.add("carbon", "buymenu")
+            confirm.classList.add("confirm", "buymenu")
+
+            const orderTotalText = document.createElement("p")
+            const orderTotalValue = document.createElement("p")
+            orderTotalText.textContent = "Order Total"
+            orderTotalValue.textContent = "0.00"
+            orderTotalValue.classList.add("total")
+            orderTotal.appendChild(orderTotalText)
+            orderTotal.appendChild(orderTotalValue)
+
+            const carbonImage = document.createElement("img")
+            const carbonText = document.createElement("p")
+            const strong = document.createElement("strong")
+            carbonImage.setAttribute("src", "assets/images/icon-carbon-neutral.svg")
+            carbonImage.setAttribute("alt", "Carbon Neutral")
+            carbonText.textContent = "This is a "
+            strong.textContent = "carbon-neutral"
+            const nodeText = document.createTextNode(" delivery")
+            carbonText.appendChild(strong)
+            carbonText.appendChild(nodeText)
+            carbon.appendChild(carbonImage)
+            carbon.appendChild(carbonText)
+
+            const buttonName = document.createElement("p")
+            buttonName.textContent = "Confirm Order"
+            confirm.appendChild(buttonName)
+
+
+            cart.appendChild(orderTotal)
+            cart.appendChild(carbon)
+            cart.appendChild(confirm)
+        }
+             
+         const orderTotalElement = document.querySelector("p.total")
+         let orderTotalCart = parseFloat(orderTotalElement.textContent.replace(/[^\d,.-]/g, ''))
+         orderTotalElement.textContent = "$" + (orderTotalCart + Number(priceCurrent.textContent.replace(/[^\d,.-]/g, ''))).toFixed(2)
 
         const incrementB = selection.querySelector("button.increment")
         incrementB.addEventListener("click", function() {
             let quantity = selection.querySelector("p.quantity")
 
             quantity.innerHTML = Number(quantity.innerHTML) + 1
+            const currentQuantity = Number(multi.textContent.replace(/[^\d,.-]/g, '')) + 1
+            multi.textContent = currentQuantity + "x"
+
+            quantityCartTotal.textContent = Number(quantityCartTotal.textContent) + 1
+            
+            const priceTotalCurrent = (parseFloat(priceCurrent.textContent.replace(/[^\d,.-]/g, '')) * currentQuantity).toFixed(2)
+            priceTotal.textContent = "$" + priceTotalCurrent
+
+            orderTotalCart = parseFloat(orderTotalElement.textContent.replace(/[^\d,.-]/g, ''))
+            orderTotalElement.textContent = "$" + (orderTotalCart + Number(priceCurrent.textContent.replace(/[^\d,.-]/g, ''))).toFixed(2)
+
         })
 
         const decrementB = selection.querySelector("button.decrement")
         decrementB.addEventListener("click", function() {
             let quantity = selection.querySelector("p.quantity")
 
-            if (Number(quantity.innerHTML) > 0)
+            if (Number(quantity.innerHTML) > 1)
             {
                 quantity.innerHTML = Number(quantity.innerHTML) - 1
+                const currentQuantity = Number(multi.textContent.replace(/[^\d,.-]/g, '')) - 1
+                multi.textContent = currentQuantity + "x"
+
+                quantityCartTotal.textContent = Number(quantityCartTotal.textContent) - 1
+
+                const priceTotalCurrent = (parseFloat(priceCurrent.textContent.replace(/[^\d,.-]/g, '')) * currentQuantity).toFixed(2)
+                priceTotal.textContent = "$" + priceTotalCurrent
             }
 
             
         })
-    }
-
-    if (buttonCurrent.id === "addbrulee")
-    {
-        const selection = document.querySelector("div#selectionbrulee")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addmacaron")
-    {
-        const selection = document.querySelector("div#selectionmacaron")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addtiramisu")
-    {
-        const selection = document.querySelector("div#selectiontiramisu")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addbaklava")
-    {
-        const selection = document.querySelector("div#selectionbaklava")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addpie")
-    {
-        const selection = document.querySelector("div#selectionpie")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addcake")
-    {
-        const selection = document.querySelector("div#selectioncake")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addbrownie")
-    {
-        const selection = document.querySelector("div#selectionbrownie")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-    if (buttonCurrent.id === "addpanna")
-    {
-        const selection = document.querySelector("div#selectionpanna")
-        selection.style.display = "flex"
-
-        const incrementB = selection.querySelector("button.increment")
-        incrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            quantity.innerHTML = Number(quantity.innerHTML) + 1
-        })
-
-        const decrementB = selection.querySelector("button.decrement")
-        decrementB.addEventListener("click", function() {
-            let quantity = selection.querySelector("p.quantity")
-
-            if (Number(quantity.innerHTML) > 0)
-            {
-                quantity.innerHTML = Number(quantity.innerHTML) - 1
-            }
-
-            
-        })
-    }
-
-
 }
